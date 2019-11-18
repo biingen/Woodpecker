158e92f6e08865e25936212bf98d3b45e416bc5c
biingen <biingen@gmail.com>
2019-11-18 17:10:13 +0800

    Release v1.0.06 version.

--- branches/Woodpecker_All/Woodpecker/Woodpecker/Form1.Designer.cs
+++ branches/Woodpecker_All/Woodpecker/Woodpecker/Form1.Designer.cs
@@ -290,12 +290,12 @@ namespace Woodpecker
             this.label_FwVersion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label_FwVersion.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(102)))), ((int)(((byte)(121)))));
             this.label_FwVersion.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.label_FwVersion.Location = new System.Drawing.Point(2, 93);
+            this.label_FwVersion.Location = new System.Drawing.Point(2, 94);
             this.label_FwVersion.Margin = new System.Windows.Forms.Padding(2);
             this.label_FwVersion.Name = "label_FwVersion";
-            this.label_FwVersion.Size = new System.Drawing.Size(61, 17);
+            this.label_FwVersion.Size = new System.Drawing.Size(78, 17);
             this.label_FwVersion.TabIndex = 46;
-            this.label_FwVersion.Text = "V 1.0.05";
+            this.label_FwVersion.Text = "Ver. 1.0.06";
             this.label_FwVersion.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.label_FwVersion.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.LabelVersion_MouseClick);
             // 
