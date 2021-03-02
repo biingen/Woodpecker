778ee7da0ad99679589abb93209152cf46971740
pingenhsieh <pingen.hsieh@auo.com>
2021-03-02 13:16:20 +0800

    Fixed to display Can-bus don't install message when pressing the stop button.

--- branches/Woodpecker_All/Woodpecker/Woodpecker/Form1.cs
+++ branches/Woodpecker_All/Woodpecker/Woodpecker/Form1.cs
@@ -10856,10 +10856,13 @@ namespace Woodpecker
                     timer_countdown.Stop();//停止倒數//
                     CloseDtplay();//關閉DtPlay//
                     duringTimer.Enabled = false;
-                    can_send = 0;
-                    set_timer_rate = false;
-                    can_rate.Clear();
-                    can_data.Clear();
+                    if (ini12.INIRead(MainSettingPath, "Device", "UsbCANExist", "") == "1" || ini12.INIRead(MainSettingPath, "Device", "CAN1630AExist", "") == "1")
+                    {
+                        can_send = 0;
+                        set_timer_rate = false;
+                        can_rate.Clear();
+                        can_data.Clear();
+                    }
                     Serialportsave("Debug");
 
                     if (ini12.INIRead(MainSettingPath, "Port A", "Checked", "") == "1")
@@ -11027,11 +11030,13 @@ namespace Woodpecker
                     timer_countdown.Stop();  //停止倒數
                     CloseDtplay();
                     duringTimer.Enabled = false;
-
-                    can_send = 0;
-                    set_timer_rate = false;
-                    can_rate.Clear();
-                    can_data.Clear();
+                    if (ini12.INIRead(MainSettingPath, "Device", "UsbCANExist", "") == "1" || ini12.INIRead(MainSettingPath, "Device", "CAN1630AExist", "") == "1")
+                    {
+                        can_send = 0;
+                        set_timer_rate = false;
+                        can_rate.Clear();
+                        can_data.Clear();
+                    }
                     Serialportsave("Debug");
 
                     if (ini12.INIRead(MainSettingPath, "Port A", "Checked", "") == "1")
